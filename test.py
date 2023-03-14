7f73edca0f9d8bf8bc13f42bdec7ac6d3eaf34a7
jie <f5666889092@gmail.com>
2023-03-14 23:08:56 +0800

    modify

--- test.py
+++ test.py
@@ -13,13 +13,14 @@ robjects.r('''
     library(dplyr)
     library(ggplot2)
     library(httr)
-    library(biomaRt, lib="./win-library/4.1/")
     library(clusterProfiler, lib="./win-library/4.1/")
+    library(biomaRt, lib="./win-library/4.1/")
+
     library(DEP, lib="./win-library/4.1/")
     library(DOSE, lib="./win-library/4.1/")
     library(enrichplot, lib="./win-library/4.1/")
     library(NormalyzerDE, lib="./win-library/4.1/")
-    library(SummarizedExperiment)
+    library(SummarizedExperiment, lib="./win-library/4.1/")
 ''')
 
 # 在網頁顯示print的內容
