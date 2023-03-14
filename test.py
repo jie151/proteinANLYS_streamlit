c103f73451f2c8b24b2894306714262c6192f1af
jie <f5666889092@gmail.com>
2023-03-14 23:12:55 +0800

    test

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ robjects.r('''
     library(enrichplot, lib="./win-library/4.1/")
     library(NormalyzerDE, lib="./win-library/4.1/")
     library(SummarizedExperiment, lib="./win-library/4.1/")
-    library(biomaRt, lib="./win-library/4.1/")
+   # library(biomaRt, lib="./win-library/4.1/")
 
 
 ''')
