e7a3f0c6a06ee89541306163c6b6204853cca472
jie <f5666889092@gmail.com>
2023-03-15 12:38:33 +0800

    test

--- website.py
+++ website.py
@@ -9,7 +9,7 @@ from io import StringIO
 
 cmd = 'whoami'
 os.system("apt-get install sudo")
-os.system("sudo chmod 777 -R /usr/local/lib/R/site-library")
+os.system("chmod 777 -R /usr/local/lib/R/site-library")
 # Load package in r
 robjects.r('''
 
