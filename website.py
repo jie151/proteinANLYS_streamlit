e50ae4e561f756145acfe53c0bc204a93cbfecae
jie <f5666889092@gmail.com>
2023-03-18 22:52:22 +0800

    rlang > 1.0.0

--- website.py
+++ website.py
@@ -41,7 +41,7 @@ robjects.r('''
     library(DEP, lib = path)
     library(DOSE, lib = path)
     library(enrichplot, lib = path)
-    install.packages("rlang", lib=path)
+    install.packages("rlang", lib=path, version= "1.0.0")
     BiocManager::install("NormalyzerDE", lib = path, force=TRUE, update=FALSE, ask=FALSE)
     library(NormalyzerDE, lib = path)
     library(SummarizedExperiment, lib = path)
