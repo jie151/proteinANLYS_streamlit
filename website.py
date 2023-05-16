0d97ee9076ef65f6695fe14237aa2a287786a814
jie <f5666889092@gmail.com>
2023-05-16 23:30:01 +0800

    fix

--- website.py
+++ website.py
@@ -1,9 +1,4 @@
 import os
-
-
-os.environ['R_HOME']= 'C:\\Program Files\\R\\R-4.2.3'
-os.environ['PATH'] += os.pathsep + 'C:\\Program Files\\R\\R-4.2.3\\bin\\X64\\'
-os.environ['PATH'] += os.pathsep + 'C:\\Program Files\\R\\R-4.2.3\\'
 from rpy2 import robjects
 import streamlit as st
 import pandas as pd
